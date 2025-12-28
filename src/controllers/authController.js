a9188a149fc18a7a2aa80bc55665817a36489f81
Bhuviofficial <bhuvi1928.g@gmail.com>
2025-12-28 17:27:36 +0530

    last comit

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -33,13 +33,15 @@ export const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email });
-    if (!user)
-      return res.status(401).json({ message: "Invalid credentials" });
+ const user = await User.findOne({ email });
+if (!user) {
+  return res.status(404).json({ message: "User not found" });
+}
 
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch)
-      return res.status(401).json({ message: "Invalid credentials" });
+const isMatch = await bcrypt.compare(password, user.password);
+if (!isMatch) {
+  return res.status(400).json({ message: "Invalid credentials" });
+}
 
     res.json({
       _id: user._id,
