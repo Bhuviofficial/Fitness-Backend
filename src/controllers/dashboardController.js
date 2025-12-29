da447feb8f059611cfea9a04fc1809277ee15045
Bhuviofficial <bhuvi1928.g@gmail.com>
2025-12-29 11:46:54 +0530

    controllers added

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -10,7 +10,7 @@ export const getDashboardData = async (req, res) => {
         workout: 45
       }
     });
-  } catch (err) {
-    res.status(500).json({ message: "Dashboard fetch failed" });
+  } catch (error) {
+    res.status(500).json({ message: "Dashboard data error" });
   }
 };
