d78a63aa986110f03fd0cf009d84b45ee1b31358
Bhuviofficial <bhuvi1928.g@gmail.com>
2026-01-07 12:45:18 +0530

    dashboard

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,25 +1,15 @@
-import Nutrition from "../models/Nutrition.js";
-import Exercise from "../models/Exercise.js";
-
 export const getDashboardData = async (req, res) => {
   try {
-    const userId = req.user.id;
-
-    const nutrition = await Nutrition.find({ user: userId });
-    const exercises = await Exercise.find({ user: userId });
-
-    const totalCalories = nutrition.reduce(
-      (sum, item) => sum + item.calories,
-      0
-    );
-
-    const totalWorkouts = exercises.length;
-
     res.status(200).json({
       success: true,
-      totalCalories,
-      totalWorkouts,
-      mealsLogged: nutrition.length,
+      stats: {
+        stepsToday: 8450,
+        caloriesToday: 520,
+        water: 2.5,
+        workout: 45,
+      },
+      weeklySteps: [6200, 7500, 8000, 9000, 8500, 9200, 10000],
+      weeklyCalories: [400, 450, 500, 550, 520, 600, 650],
     });
   } catch (error) {
     res.status(500).json({
