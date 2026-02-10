f26aee50b879b828c3b7ce4b0dad69063fa42647
Bhuviofficial <bhuvi1928.g@gmail.com>
2026-02-10 16:00:12 +0530

    #

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
-import { registerUser, loginUser } from "../controllers/authController.js";
+import {loginUser } from "../controllers/authController.js";
 import { register } from "../controllers/registerController.js";
 
 const router = express.Router();
-
-router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/register", register); 
 
